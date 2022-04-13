2b151a898ed2d512448da664bf722932bdd3da43
Anupam kumar <anupamit011@gmail.com>
2022-04-13 18:25:34 +0530

    find alphabets

---
+++ string/findalphabet.js
@@ -0,0 +1,7 @@
+function findAlphabet(sentence) {
+    return sentence === sentence.toUpperCase()
+} 
+console.log(findAlphabet('amoop'));
+console.log(findAlphabet('Amoop'));
+console.log(findAlphabet('ANOOP'));
+console.log(findAlphabet('anOOp'));
